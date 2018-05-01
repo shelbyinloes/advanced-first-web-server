599486e5527b1a54bc5366dfa3758afa990248c5
Shelby Inloes <shelbyinloes@gmail.com>
2018-04-30 20:51:55 -0500

    get, post, delete

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 let express = require("express");
+let state = require("./state")
+let users = state.users
 
 const app = express();
 
 app.use(express.static('public'))
+
+app.get("/users",function(req,res,next){
+ return res.json(users);
+});
+
+app.post("/users",function(req,res,next){
+ return res.json(users.push({}));
+});
+
+app.delete("/users",function(req,res,next)
+{
+ return res.json(users.pop({}));
+});
 
 app.listen(3002, (err) => {
 if (err) {
