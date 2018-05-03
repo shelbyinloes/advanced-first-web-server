8003bd1a39a7d316c3003a669757550a34f244d4
Shelby Inloes <shelbyinloes@gmail.com>
2018-05-03 13:25:36 -0500

    added in route & controller files, updated get & post

---
+++ controllers/ContactController.js
@@ -0,0 +1,16 @@
+module.exports.list =  function list(request, response) {
+    return response.json(users);
+}
+module.exports.show =  function show(request, response) {
+    return response.json({theId: request.params.id});
+}
+module.exports.create =  function create(request, response) {
+    return response.json({});
+}
+module.exports.update =  function update(request, response) {
+    return response.json({theId: request.params.id});
+}
+module.exports.remove =  function remove(request, response) {
+    return response.json({});
+}
+   
--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 let express = require("express");
 let state = require("./state")
 let users = state.users
-
 const app = express();
-
+let bodyParser = require("body-parser");
+let ContactRoutes = require("./routes/ContactRoutes");
+app.use(ContactRoutes);
+app.use(bodyParser.json());
 app.use(express.static('public'))
 
-app.get("/users",function(req,res,next){
- return res.json(users);
-});
 
-app.post("/users",function(req,res,next){
- return res.json(users.push({}));
-});
+// app.get("/users",function(req,res,next){
+//  return res.json(users);
+// });
 
-app.delete("/users",function(req,res,next)
-{
- return res.json(users.pop({}));
-});
+app.get("/users/:userId", function (request, response, next){
+  // let userId = request.params.userId
+  // response.json(users.find(function(user){
+  //   return user._id == request.params.userId;
+  // }))
 
-app.listen(3002, (err) => {
+  response.json(users.find(user=>user._id == request.params.userId))
+})
+
+// app.post("/users",function(req,res,next){
+//  return res.json(users.push({}));
+// });
+
+app.post("/users", function(request,response,next){
+  let newUser = request.body;
+  users.push(newUser);
+  response.json(newUser);
+})
+
+// app.delete("/users",function(req,res,next)
+// {
+//  return res.json(users.pop({}));
+// });
+
+app.listen(5888, (err) => {
 if (err) {
   return console.log("Error", err);
 }
-console.log("Web server is now living in apartment 3002");
+console.log("Web server is now living in apartment 5888");
 });
